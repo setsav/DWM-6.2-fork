3d351b769e0bb58b167a1ca29a718d73a9c5bb3e
setsav <poopshit@tuta.io>
2022-03-01 13:41:18 -0500

    added volume notifications

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* audio constants */
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",   NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",   NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", "&", "sh", ".dwm/master.sh", "volumenotify", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", "&", "sh", ".dwm/master.sh", "volumenotify", NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL };
 
 /* brightness constants */
